adc4e40c9f8feafe404f511241a9655668f68b59
FishmanDev <fishman.code@gmail.com>
2017-08-14 10:48:17 +0300

    Update the current active tab with '.zone' domain only

--- background.js
+++ background.js
@@ -2,6 +2,10 @@
 var ws = new WebSocket("ws://live-reloader.dev:3000")
 ws.onmessage = (message) => {
   if (message.data == 'reload') {
-    chrome.tabs.reload()
+    chrome.tabs.query({active: true, url: 'http://*.zone/*'}, (tabs) => {
+      if (tabs[0]) {
+        chrome.tabs.reload(tabs[0].id)
+      }
+    })
   }
 }
